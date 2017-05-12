ad19ce16eae5b49e5a9940aac4a4033662db5c04
alandward <alan.ward.4@gmail.com>
2017-05-12 14:00:23 -0600

    Converting over to simpler linmath utils... glm had hundreds more files
    zz

--- app/src/main/vkutils/util.hpp
+++ app/src/main/vkutils/util.hpp
@@ -29,8 +29,9 @@
 #include <vector>
 
 #define GLM_FORCE_RADIANS
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
+//#include "glm/glm.hpp"
+//#include "glm/gtc/matrix_transform.hpp"
+#include "linmath.h"
 
 #ifdef _WIN32
 #pragma comment(linker, "/subsystem:console")
